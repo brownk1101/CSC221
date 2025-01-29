21bee0aed99e3da005446098d29e586af5b2773a
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-29 00:01:28 -0500

    feat(M2Lab): Stage 1 complete

--- Mod2/lab/M2Lab_Panda_DF_HarleyCoughlin.py
+++ Mod2/lab/M2Lab_Panda_DF_HarleyCoughlin.py
@@ -5,7 +5,42 @@
 
 import pandas as pd
 
+
 def main():
+    """Entry point to the program. Calls all other functions"""
+
+    error = False
+    done = False
+
+    while not error and not done:
+        print("Stage 1: Loading and getting information")
+        print("Loading djia.csv")
+
+        # My LSP kept yelling at me that djia was possibly unbound.
+        djia: pd.DataFrame = pd.DataFrame()
+
+        try:
+            djia = pd.read_csv("./djia.csv", index_col="Symbol")
+        except FileNotFoundError:
+            print("Couldn't find djia.csv in the current directory")
+            error = True
+
+        print("djia.csv loaded")
+        print(djia)
+        print()
+
+        print("PD info:")
+        djia.info()
+        print()
+
+        print(f"Length: {len(djia)}")
+        print(f"Shape: {djia.shape}")
+        print()
+
+        print(f"First row: {djia.head(1)}")
+        print(f"Last row: {djia.tail(1)}")
+
+        done = True
     ...
 
 
