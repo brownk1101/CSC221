69e7865b86afcf785c28d290ee7b2061be6f7d89
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-29 11:01:07 -0500

    feat(M2Pro1): Initial

---
+++ Mod2/pro/M2Pro1_Panda_DF_HarleyCoughlin.py
@@ -0,0 +1,12 @@
+# This a menu driven program using DataFrames, exception handling, functions, and file processing
+# 01/29/2025
+# CSC221 M2Pro1-Panda DF
+# Harley Coughlin
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
