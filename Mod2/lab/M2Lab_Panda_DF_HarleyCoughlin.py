f71d707f207e0f245180287d4af27d52e48ed8f7
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-29 00:05:46 -0500

    refactor(M2Lab): Break stage one into function

--- Mod2/lab/M2Lab_Panda_DF_HarleyCoughlin.py
+++ Mod2/lab/M2Lab_Panda_DF_HarleyCoughlin.py
@@ -5,6 +5,29 @@
 
 import pandas as pd
 
+def stage_one() -> pd.DataFrame:
+    print("Stage 1: Loading and getting information")
+    print("Loading djia.csv")
+
+    djia = pd.read_csv("./djia.csv", index_col="Symbol")
+
+    print("djia.csv loaded")
+    print(djia)
+    print()
+
+    print("PD info:")
+    djia.info()
+    print()
+
+    print(f"Length: {len(djia)}")
+    print(f"Shape: {djia.shape}")
+    print()
+
+    print(f"First row: {djia.head(1)}")
+    print(f"Last row: {djia.tail(1)}")
+
+    return djia
+
 
 def main():
     """Entry point to the program. Calls all other functions"""
@@ -13,32 +36,12 @@ def main():
     done = False
 
     while not error and not done:
-        print("Stage 1: Loading and getting information")
-        print("Loading djia.csv")
-
-        # My LSP kept yelling at me that djia was possibly unbound.
-        djia: pd.DataFrame = pd.DataFrame()
-
         try:
-            djia = pd.read_csv("./djia.csv", index_col="Symbol")
+            stage_one()
         except FileNotFoundError:
             print("Couldn't find djia.csv in the current directory")
             error = True
 
-        print("djia.csv loaded")
-        print(djia)
-        print()
-
-        print("PD info:")
-        djia.info()
-        print()
-
-        print(f"Length: {len(djia)}")
-        print(f"Shape: {djia.shape}")
-        print()
-
-        print(f"First row: {djia.head(1)}")
-        print(f"Last row: {djia.tail(1)}")
 
         done = True
     ...
