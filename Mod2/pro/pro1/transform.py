c3d89e62d7409e481a0bd8b045a14824d173c562
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-31 11:36:54 -0500

    feat(M2Pro1): get_survivors complete

--- Mod2/pro/pro1/transform.py
+++ Mod2/pro/pro1/transform.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 
-def get_survivors(data: pd.DataFrame, who: str = "Both") -> tuple[int, int]:
+def get_survivors(data: pd.DataFrame, who: str = "Both"):
     """Extracts information on survived vs dead for a given group of passengers
 
     Prameters
@@ -12,14 +12,33 @@ def get_survivors(data: pd.DataFrame, who: str = "Both") -> tuple[int, int]:
         The DataFrame to extract information from.
     who: str (default = "Both")
         The group of passengers to extract information about.
+
+    Returns
+    -------
+    tuple
+        if who="Both", returns a tuple of ((female_alive, female_dead),
+        (male_alive, male_dead))
+        Otherwise returns a tuple of (alive, dead)
     """
-    to_return: pd.DataFrame = pd.DataFrame()
-    amount_survived = 0
-    amount_dead = 0
-    match who:
-        case "Both":
-            raise NotImplementedError
-        case "Females Survived":
-            raise NotImplementedError
-        case "Males Survived":
-            raise NotImplementedError
+    if who == "Both":
+        return (get_survivors(data, "Females Survived"),
+                get_survivors(data, "Males Survived"))
+    else:
+        if who == "Females Survived":
+            filter = "female"
+        elif who == "Males Survived":
+            filter = "male"
+        else:
+            raise ValueError("""'who' must be 'Both', 'Females Survived', or
+                                'Males Survived'""")
+        people = data[data.gender == filter]["survived"]
+        amount_alive = 0
+        amount_dead = 0
+        # Count survived(1) and dead(0)
+        for person in people:
+            if person == 1:
+                amount_alive += 1
+            else:
+                amount_dead += 1
+
+        return (amount_alive, amount_dead)
