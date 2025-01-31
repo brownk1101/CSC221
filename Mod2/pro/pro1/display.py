936386cc6a28950ec0153b6a1222812732810e9e
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-31 11:49:12 -0500

    feat(M2Pro1_display): Add survivor amounts function

--- Mod2/pro/pro1/display.py
+++ Mod2/pro/pro1/display.py
@@ -12,7 +12,9 @@ def first_fifteen(data: pd.DataFrame, amount: int = 15) -> None:
     amount: int (default 15)
         The amount of rows from the top to display.
     """
+    print()
     print(data.head(amount))
+    print()
 
 
 def records_amount(data: pd.DataFrame) -> None:
@@ -23,17 +25,34 @@ def records_amount(data: pd.DataFrame) -> None:
     data: DataFrame
         The DataFrame to display the amount of records
     """
+    print()
     print(f"Amount of passengers: {len(data)}")
+    print()
 
 
-def survivor_amounts(data: tuple[int], separate: bool = False) -> None:
+def survivor_amounts(people, who: str = "All") -> None:
     """Prints the amount of survivors vs dead for a given group of people.
 
     Parameters
     ----------
-    data: tuple[int]
+    data: tuple[int, int] | tuple[tuple[int, int], tuple[int, int]]
         The survivor amounts to display.
-    separate: bool (default = False)
-        Whether or not to separate the groups information.
+    who: str
+        The group of passengers to print information about.
+        Options: "All", "Both", "Females Survived", "Males Survived"
     """
-    raise NotImplementedError
+    print()
+    match who:
+        case "All":
+            survivors = people[0][0] + people[1][0]
+            dead = people[0][1] + people[1][1]
+            print(f"Survivors: {survivors}, Dead: {dead}")
+        case "Both":
+            print(f" Female Survivors: {people[0][0]}, Female  Dead: {people[0][1]}")
+            print(f" Male Survivors: {people[1][0]}, Male  Dead: {people[1][1]}")
+        case "Females Survived":
+            print(f" Female Survivors: {people[0][0]}, Female  Dead: {people[0][1]}")
+        case "Males Survived":
+            print(f" Male Survivors: {people[0][0]}, Male  Dead: {people[0][1]}")
+
+    print()
