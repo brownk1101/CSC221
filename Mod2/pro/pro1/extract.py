5c6ea51f036f1038e6872a0469ad7cdf164e7de6
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-01-31 07:54:20 -0500

    docs(M2Pro1): Add docstring to function in extract

--- Mod2/pro/pro1/extract.py
+++ Mod2/pro/pro1/extract.py
@@ -1,9 +1,21 @@
-"""This module is for data extraction"""
+"""This module is for data extraction."""
 
 import os
 import pandas as pd
 
 def extract_data(filename: str) -> pd.DataFrame:
+    """Extract data from an Excel file into a DataFrame.
+
+    Parameters
+    ----------
+    filename: str
+        The filename of the excel file to extract data from.
+
+    Returns
+    -------
+    titanic: pd.DataFrame
+        The resulting DataFrame extracted from the excel file.
+    """
     current_directory = os.getcwd()
     filepath = os.path.join(current_directory, filename)
     titanic = pd.read_excel(filepath)
