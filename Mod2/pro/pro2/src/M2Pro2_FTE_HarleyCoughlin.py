57f63d2318d7cd22cd668ae5085e4b10511a3c2e
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-10 08:48:59 -0500

    feat(M2Pro2): Main start

--- Mod2/pro/pro2/src/M2Pro2_FTE_HarleyCoughlin.py
+++ Mod2/pro/pro2/src/M2Pro2_FTE_HarleyCoughlin.py
@@ -2,3 +2,26 @@
 # 02/09/2025
 # CSC221 M2Pro2 - FTE
 # Harley Coughlin
+
+import pandas as pd
+import menu
+import extract
+
+
+def main():
+    df = pd.DataFrame()
+    filename = "deansDailyCsar.csv"
+    try:
+        df = extract.extract_csv(filename=filename)
+    except FileNotFoundError as e:
+        print(f"""Unable to find the {filename}. Please make sure it exists in
+                 the results/input directory.""")
+        print(f"Technical details: {e}")
+    except pd.errors.EmptyDataError as e:
+        print(f"The input file {filename} is empty. Please check the file contents.")
+        print(f"Technical details: {e}")
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
