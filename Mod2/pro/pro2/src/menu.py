ce68ced534b396636356657236d828ecf135d033
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-10 07:13:11 -0500

    feat(M2Pro2): Add main menu print function

---
+++ Mod2/pro/pro2/src/menu.py
@@ -0,0 +1,10 @@
+"""Menu Functions"""
+
+def main_menu() -> None:
+    print()
+    print(f"{'Main Menu':-^35}")
+    print("1. Enter \"Sec Divisions\" code")
+    print("2. Get Course Enrollment Percentage")
+    print("3. FTE by Division")
+    print("4. FTE per Instructor")
+    print("5. FTE per course")
