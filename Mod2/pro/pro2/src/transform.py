26e2b1cb3e59b356d5348f7bfae6288f7e084638
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-12 07:30:49 -0500

    feat(M2Pro2.transform): Add sort and change get_division_codes

---
+++ Mod2/pro/pro2/src/transform.py
@@ -0,0 +1,42 @@
+"""Data transformations"""
+
+import pandas as pd
+
+
+def sort_dataframe(data: pd.DataFrame, sort_by: list[str] =
+                   ["Sec Divisions", "Sec Name", "Sec Faculty Info"]
+                   ) -> pd.DataFrame:
+    """Sorts a DataFrame by columns, in ascending order.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        DataFrame to sort.
+    sort_by: list[str]
+        Column name(s) to sort by.
+        Default = ["Sec Divisions", "Sec Name", "Sec Faculty Info"]
+
+    Returns
+    -------
+    pd.DataFrame
+        Sorted DataFrame
+    """
+    return data.sort_values(by=sort_by)
+
+
+def get_division_codes(data: pd.DataFrame) -> list[str]:
+    """Extracts unique division codes from a DataFrame.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        DataFrame to read division codes from.
+
+    Returns
+    -------
+    list[str]
+        List of unique division codes.
+    """
+    unique_values = data["Sec Divisions"].dropna().unique()
+    # Explicit conversion to list[str] so my lsp will stop yelling at me.
+    return [str(x) for x in unique_values]
