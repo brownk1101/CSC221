498143a3d4881cf3b05aa6549f2ea3484731a171
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-13 09:50:20 -0500

    docs(M2Pro2.load): Add docstring for excel sheets

--- Mod2/pro/pro2/src/load.py
+++ Mod2/pro/pro2/src/load.py
@@ -5,6 +5,7 @@ import os.path
 
 
 def create_excel_sheets(data, name: str) -> None:
+    """Writes excel files to resources/output/"""
     file_path = os.path.join("resources", "output")
     file_path = os.path.join(file_path, name + ".xlsx")
     data.to_excel(excel_writer=file_path)
