f73a0b3e611d02b50dd566000c56ad1328eec39c
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-02-27 07:16:17 -0500

    refactor(Mod3): restructure

---
+++ Mod3/pro/etl.py
@@ -0,0 +1,21 @@
+"""Recipe extraction function"""
+
+
+import os
+import pandas as pd
+
+
+def extract_recipes(file_name: str) -> pd.DataFrame:
+    """Extracts recipes from an excel sheet located in the current directory.
+
+    Params:
+    file_name: str
+        Name of the excel file to exctract from.
+    """
+    file_path = os.path.join(os.getcwd(), file_name)
+    # If the file doesn't exist in the current directory, raise an error.
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError
+
+    recipes = pd.read_excel(file_path)
+    return recipes
---
+++ Mod3/pro/recipe.py
@@ -0,0 +1,75 @@
+"""Recipe class and subclasses."""
+
+
+class Recipe:
+    def __init__(self, name: str, ingredients: list[str], steps: list[str],
+                 is_vegetarian: bool):
+        """Recipe class constructor.
+
+        Params
+        ------
+        name: str
+            Name of the recipe.
+        ingredients: list[str]
+            List of ingredients for the recipe.
+        steps: list[str]
+            Ordered list of steps to create the recipe.
+        is_vegetarian: bool
+            True if the recipe is a vegetarian recipe.
+        """
+        self.__name = name
+        self.__ingredients = ingredients
+        self.__steps = steps
+        self.__is_vegetarian = is_vegetarian
+
+
+    def get_name(self) -> str:
+        """Returns the name of the recipe."""
+        return self.__name
+
+
+    def get_ingredients(self) -> list[str]:
+        """Returns the list ingredients of the recipe."""
+        return self.__ingredients
+
+
+    def get_steps(self) -> list[str]:
+        """Returns the list of steps in order."""
+        return self.__steps
+
+
+    def is_vegetarian(self) -> bool:
+        """Returns whether the recipe is vegetarian or not."""
+        return self.__is_vegetarian
+
+
+    def set_name(self, name: str) -> None:
+        """Sets the name of the recipe."""
+        self.__name = name
+
+    
+    def set_ingredients(self, ingredients: list[str]) -> None:
+        """Sets the list of ingredients for the recipe."""
+        self.__ingredients = ingredients
+
+
+    def set_steps(self, steps: list[str]) -> None:
+        """Sets the steps for the recipe."""
+        self.__steps = steps
+
+
+    def set_vegetarian(self, is_vegetarian: bool) -> None:
+        """Sets whether the recipe is vegetarian or not."""
+        self.__is_vegetarian = is_vegetarian
+
+
+    def display_recipe(self):
+        """Prints formatted recipe."""
+        print(f"{self.__name: ^40}")
+        print(f"{'':-^40}")
+        print("Ingredients:")
+        for ingredient in self.__ingredients:
+            print(f"- {ingredient}")
+        for count, step in enumerate(self.__steps):
+            print(f"Step #{count + 1}")
+            print(f"{'': >4}{step}")
